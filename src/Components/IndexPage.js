a87e0fcd08bec6e8a986e86d5a53431a7ffe2067
unknown <dacharya294@gmail.com>
2024-02-06 03:05:55 +0530

    Second commit

--- src/Components/IndexPage.js
+++ src/Components/IndexPage.js
@@ -1,12 +1,29 @@
+import IndexPageImageContainerComponent from "../SubComponents/IndexPageImageContainerComponent";
+import IndexPageTurfComponent from "../SubComponents/IndexPageTurfComponent";
+
 export default function IndexPage(){
     return(
-        <div className="index_page_img_container">
-            <img className="index_page_img" src={"https://res.cloudinary.com/dfcsc86hq/image/upload/v1706971148/e7coklvzwfb8hz05vsfv.jpg"}></img>
-            <div className="index_page_img_text-content">
-               <div className="index_page_img_text-content_line1">REDISCOVER YOUR LOVE FOR THE BEAUTIFUL GAME</div>
-               <div className="index_page_img_text-content_line2">The passion, the highs of winning, the friendships that last a lifetime. Rediscover your love of the beautiful game.</div>
+       
+        <>    
+            <div className="index_page_img_container">
+                
+                <IndexPageImageContainerComponent></IndexPageImageContainerComponent>
+                
+            </div>
+
+            <div className="index_page_turf_container">
+                
+                <div className="index_page_turf_container_heading">PLAY AT NEARBY TURF</div>
+
+                <div className="index_page_turf_container_slider">
+                               
+                    <IndexPageTurfComponent></IndexPageTurfComponent>
+                    <IndexPageTurfComponent></IndexPageTurfComponent>
+       
+                </div>
             </div>
             
-        </div>
+        </>
+       
     )
 }
---
+++ src/SubComponents/IndexPageImageContainerComponent.js
@@ -0,0 +1,50 @@
+export default function IndexPageImageContainerComponent(){
+    return (
+
+        <>
+            <img className="index_page_img" src={"https://res.cloudinary.com/dfcsc86hq/image/upload/v1706971148/e7coklvzwfb8hz05vsfv.jpg"}></img>
+            <div className="index_page_img_content">
+                
+                <div className="index_page_img_text_content">
+                    <div className="index_page_img_text-content_line1">REDISCOVER YOUR LOVE FOR THE BEAUTIFUL GAME</div>
+                    <div className="index_page_img_text-content_line2">The passion, the highs of winning, the friendships that last a lifetime. Rediscover your love of the beautiful game.</div>
+                </div>
+
+            </div>    
+            <div className="index_page_img_feature_content">
+                <div className="index_page_img_feature_content_tag">
+                    <div className="index_page_img_feature_content_tag_text">
+                        HOST A GAME
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="index_page_img_feature_content_svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+
+                    
+                </div>
+                <div className="index_page_img_feature_content_tag">
+                    <div className="index_page_img_feature_content_tag_text">
+                        FIND A GAME
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="index_page_img_feature_content_svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+
+                    
+                </div>
+                <div className="index_page_img_feature_content_tag">
+                    <div className="index_page_img_feature_content_tag_text">
+                        ENJOY GAME
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="index_page_img_feature_content_svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+
+                    
+                </div>
+            </div>
+
+        </>
+
+    )
+}
